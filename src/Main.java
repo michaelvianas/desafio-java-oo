3fe2b7f8ae5c10c6abcc1253f5cacac6b9f1b105
michaelviana <vmichael116@gmail.com>
2022-12-31 13:39:30 -0300

    Testando as classes através da Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -13,22 +11,50 @@ public class Main {
         curso1.setDescricao("Descrição curso java");
         curso1.setCargaHoraria(8);
 
+        Conteudo conteudo = new Curso();
+
+        List<String> palavras = new ArrayList<>();
+
         Curso curso2 = new Curso();
         curso2.setTitulo("Curso JS");
         curso2.setDescricao("Descrição curso js");
         curso2.setCargaHoraria(4);
-
-        Conteudo conteudo = new Curso();
-
-        List<String> palavras = new ArrayList<>();
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de Java");
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devMichael = new Dev();
+        devMichael.setNome("Michael");
+        devMichael.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos de Michael:" +devMichael.getConteudosInscritos());
+        devMichael.progredir();
+        devMichael.progredir();
+        devMichael.progredir();
+        System.out.println("Conteúdos inscritos de Michael:" +devMichael.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos de Michael:" +devMichael.getConteudosConcluidos());
+        System.out.println("XP de Michael:" +devMichael.calcularTotalXp());
+
+
+        System.out.println("-------------------------------------------------------------------------");
+
+        Dev devGuilherme = new Dev();
+        devGuilherme.setNome("Guilherme");
+        devGuilherme.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos de Guilherme:" +devGuilherme.getConteudosInscritos());
+        devGuilherme.progredir();
+        devGuilherme.progredir();
+        devGuilherme.progredir();
+        System.out.println("Conteúdos inscritos de Guilherme:" +devGuilherme.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos de Guilherme:" +devGuilherme.getConteudosConcluidos());
+        System.out.println("XP de Guilherme:" +devGuilherme.calcularTotalXp());
     }
 }
--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -1,9 +1,6 @@
 package br.com.dio.desafio.dominio;
 
-import java.util.LinkedHashSet;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 public class Dev {
     private String nome;
@@ -26,10 +23,13 @@ public class Dev {
     }
 
     public double calcularTotalXp() {
-        return this.conteudosConcluidos
-                .stream()
-                .mapToDouble(Conteudo::calcularXp)
-                .sum();
+        Iterator<Conteudo> iterator = this.conteudosConcluidos.iterator();
+        double soma = 0;
+        while(iterator.hasNext()){
+            double next = iterator.next().calcularXp();
+            soma += next;
+        }
+        return soma;
     }
 
     public String getNome() {
